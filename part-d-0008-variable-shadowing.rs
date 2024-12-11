4c217450212e3bb2d3414ccb8765f5caf5d82ac2
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-11 11:27:19 +0300

    Create part-d-0008-variable-shadowing.rs

---
+++ part-d-0008-variable-shadowing.rs
@@ -0,0 +1,13 @@
+fn main() {
+
+    let yearly_budget: &str = "1_256_321.58";
+
+    let yearly_budget: f32 = 1_256_321.58;
+
+    let mut yearly_budget: u64 = 1_254_900;
+
+    yearly_budget = 1_145_654;
+
+    println!("Yearly budget is $ {yearly_budget}") // Yearly budget is $ 1145654
+
+}
