eb584b6f62cd122b3c7f5abba4f9343e253b4bfb
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-17 11:51:42 +0300

    Create part-d-0011-arch-types.rs

---
+++ part-d-0011-arch-types.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    let _result: isize = -2_132_456;
+
+    let _expected_gain: usize = 32_563;
+
+    /*
+    
+    isize and usize types depend on the architecture of the computer your program is running on, 
+    which is denoted in the table as “arch”: 64 bits 
+    if you’re on a 64-bit architecture and 32 bits if you’re on a 32-bit architecture
+    
+    */
+
+}
