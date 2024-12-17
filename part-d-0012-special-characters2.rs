a923e4291a46ecf8cbd1c703e1b57cb9caf42408
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-17 12:07:31 +0300

    Create part-d-0012-special-characters2.rs

---
+++ part-d-0012-special-characters2.rs
@@ -0,0 +1,13 @@
+fn main() {
+
+    println!("Mustafa says \"A rolling stone gathers no moss...\".");
+
+    // Mustafa says "A rolling stone gathers no moss...".
+
+    // r -> path example
+
+    let file_path = r"C:\downloads";
+
+    println!("{file_path}") // C:\downloads
+
+}
