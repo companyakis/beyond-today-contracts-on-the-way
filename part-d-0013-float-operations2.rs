7f88c67f54b0469601cc17dcfebecdb40466f2e0
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-17 12:34:07 +0300

    Create part-d-0013-float-operations2.rs

---
+++ part-d-0013-float-operations2.rs
@@ -0,0 +1,21 @@
+fn main() {
+
+    //  Format Specifier
+
+    let my_num: f64 = 212.456_878_987;
+
+    println!("{my_num:.2}");
+
+    println!("{my_num:.4}");
+
+    println!("{my_num:.5}");
+
+    println!("{my_num:.7}");
+
+// 212.46
+// 212.4569   
+// 212.45688  
+// 212.4568790
+
+}
+
