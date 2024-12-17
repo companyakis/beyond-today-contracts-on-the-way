f6191478f7d67754b9a2eb0337e4000e165eb7ff
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-17 12:03:34 +0300

    Create part-d-0012-special-characters1.rs

---
+++ part-d-0012-special-characters1.rs
@@ -0,0 +1,25 @@
+fn main() {
+
+    // new line
+
+    println!("Hi there!\nLet's use Rust...");
+
+    /*
+    Hi there!        
+    Let's use Rust...
+    */
+
+    // tab
+
+    println!("Mustafa");
+    println!("\tMustafa");
+    println!("\t\tMustafa");
+
+    /* 
+    Mustafa
+        Mustafa        
+                Mustafa
+    
+    */
+
+}
