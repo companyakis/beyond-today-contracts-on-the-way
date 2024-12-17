104cd8d361ce9a61438f8a00c19796ae667751e6
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-17 12:21:39 +0300

    Create part-d-0013-float-operations1.rs

---
+++ part-d-0013-float-operations1.rs
@@ -0,0 +1,25 @@
+fn main() {
+
+    let pi1: f32 = 22.0 / 7.0;
+
+    println!("{pi1}");
+
+    let pi2: f64 = 22.0 / 7.0;
+
+    println!("{pi2}");
+
+    // some methods
+
+    println!("{}", pi2.floor());
+
+    println!("{}", pi2.ceil());
+
+    println!("{}", pi2.round());
+
+}
+
+// 3.142857
+// 3.142857142857143
+// 3
+// 4
+// 3
