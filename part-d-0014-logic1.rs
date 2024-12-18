bbc021a78c5c8ca158f57a41c40e91ad410463bd
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-18 22:09:27 +0300

    Create part-d-0014-logic1.rs

---
+++ part-d-0014-logic1.rs
@@ -0,0 +1,22 @@
+fn main() {
+
+    let c1: bool = 27 >= 12;
+
+    let c2: bool = 12 != 12;
+
+    let c3: bool = "İstanbul" == "istanbul";
+
+    let result1 = c1 && c2;
+
+    println!("Result 1: {result1}");
+
+    let result2 = c1 || c2;
+
+    println!("Result 2: {result2}");
+
+}
+
+// Result 1: false
+// Result 2: true 
+
+
