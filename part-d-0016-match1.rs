8ae0d37f4eeb87a70dd0497beb1ad18d67530264
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-23 14:53:03 +0300

    Create part-d-0016-match1.rs

---
+++ part-d-0016-match1.rs
@@ -0,0 +1,17 @@
+fn main() {
+
+    let result: u8 = 7;
+
+    match result {
+
+        1 => println!("Too bad"),
+        2 => println!("Bad"),
+        3 => println!("Normal"),
+        4 => println!("Good"),
+        5 => println!("Awesome"),
+        _ => println!("Invalid")
+        
+    }
+
+}
+
