c720299c60f60cacd2c40e68a02c3f68eccdd7da
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-23 21:19:08 +0300

    Create part-d-0019-vector1.rs

---
+++ part-d-0019-vector1.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+    let names: Vec<&str> = Vec::new();
+
+    println!("{names:?}");
+
+    let years: Vec<u16> = vec![2020, 2054, 2024];
+
+    println!("{years:?}");
+
+}
+
+// []
+// [2020, 2054, 2024]
