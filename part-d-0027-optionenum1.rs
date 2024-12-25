6ff92ce9da22fc80dc71136f47d6084d10742e1c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-25 12:03:58 +0300

    Create part-d-0027-optionenum1.rs

---
+++ part-d-0027-optionenum1.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    let year: Option<u16> = Option::Some(2025);
+
+    let name: Option<&str> = Some("Mustafa");
+
+    let is_completed: Option<bool> = Some(true);
+
+    let wedding_year: Option<u16> = Option::None;
+
+}
+
+
+
+
