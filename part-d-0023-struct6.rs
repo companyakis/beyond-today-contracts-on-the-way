fb25ad4212284ea721e7b64924e478c3bfd46439
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-26 08:41:49 +0300

    Create part-d-0023-struct6.rs

---
+++ part-d-0023-struct6.rs
@@ -0,0 +1,32 @@
+fn main() {
+
+    let mut tea_order = make_drink("Tea".to_string(), 11.25, 'm', true);
+
+    drink_comment(&mut tea_order);
+    
+    println!("{}", tea_order.name);
+
+    // Our customers like Tea.
+    // Tea
+
+}
+
+struct Drink {
+    name: String,
+    price: f32,
+    size: char, // 's', 'm', 'l
+    hot: bool
+}
+
+
+fn make_drink(name: String, price: f32, size: char, hot: bool) -> Drink {
+    Drink { name, price, size, hot}
+}
+
+// fn make_drink(name: String, price: f32, size: char, hot: bool) -> Drink {
+//     Drink { name: name, price: price, size: size, hot: hot }
+// }
+
+fn drink_comment(drink: &mut Drink) {
+    println!("Our customers like {}. ", drink.name)
+}
