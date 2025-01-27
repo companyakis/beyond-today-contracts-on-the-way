db5a8c0edabf40f9d37ecca770e5e4780d8a10d0
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-27 12:00:01 +0300

    Create part-d-0037-destructuring1.rs

---
+++ part-d-0037-destructuring1.rs
@@ -0,0 +1,19 @@
+fn main() {
+
+    let (name, id, title) = ("Mustafa Buyukdereli", 101, "Product Owner");
+
+    println!("Name: {name}");
+    println!("ID: {id}");
+    println!("Title: {title}");
+
+    let (founder_Mustafa, _) = ("Mustafa Buyukdereli", "Aygün Arslan");
+
+    println!("{founder_Mustafa}");
+
+}
+
+// Name: Mustafa Buyukdereli
+// ID: 101
+// Title: Product Owner
+// Mustafa Buyukdereli
+
