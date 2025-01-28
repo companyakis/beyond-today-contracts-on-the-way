d9881c50f8f116f39c8a3019043a4af4db48daf2
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-28 12:18:01 +0300

    Update part-d-0030-strings8.rs

--- part-d-0030-strings8.rs
+++ part-d-0030-strings8.rs
@@ -2,7 +2,7 @@ fn main() {
     
     let me = String::from("Mustafa Buyukdereli");
 
-    let first_name: &str = &me[0..7];
+    let first_name: &str = &me[0..7]; // let first_name: &str = &me[..7];
 
     let last_name: &str = &me[7..me.len()]; // let last_name: &str = &me[7..];
 
