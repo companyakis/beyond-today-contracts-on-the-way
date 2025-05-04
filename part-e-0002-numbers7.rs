e9a71fb1caf48778c03a9a4c3b73b928ba6e623d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-04 21:54:24 +0300

    Create part-e-0002-numbers7.rs

---
+++ part-e-0002-numbers7.rs
@@ -0,0 +1,13 @@
+// fn main() {
+//     assert!(0.1+0.2==0.3);
+
+//     println!("Success!");
+// }
+
+
+
+fn main() {
+    assert!(0.1 as f32+0.2 as f32==0.3 as f32);
+
+    println!("Success!");
+}
