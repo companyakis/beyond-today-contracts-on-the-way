b5afd5dd344cf4d9316a789bed01b81601bbc145
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-05 10:59:05 +0300

    Create part-e-0004-function3.rs

---
+++ part-e-0004-function3.rs
@@ -0,0 +1,26 @@
+// // Solve it in two ways
+// // DON'T let `println!` work
+// fn main() {
+//     never_return();
+
+//     println!("Failed!");
+// }
+
+// fn never_return() -> ! {
+//     // Implement this function, don't modify the fn signatures
+    
+// }
+
+// Solve it in two ways
+// DON'T let `println!` work
+fn main() {
+    never_return();
+
+    println!("Failed!");
+}
+
+fn never_return() -> ! {
+    // Implement this function, don't modify the fn signatures
+    panic!("Stop!")
+    
+}
