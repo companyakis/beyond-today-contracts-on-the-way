cb3f3112ff094418656a00e5c1c4dd66945b9272
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-06 09:32:57 +0300

    Create part-d-0040-static-array4.rs

---
+++ part-d-0040-static-array4.rs
@@ -0,0 +1,12 @@
+fn main() {
+
+    let mut family_ages: [u8; 5] = [66, 43, 35, 14, 9];
+
+    family_ages[0] = 68;
+
+    println!("Family ages: {:?}", family_ages)
+
+
+} 
+
+// Family ages: [68, 43, 35, 14, 9]
