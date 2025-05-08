bc8731a2cdcbf3e8a89e7a6f9bf81a190cb60a24
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-08 10:30:09 +0300

    Update part-c-0004-immutable.sol

--- part-c-0004-immutable.sol
+++ part-c-0004-immutable.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
 
+contract CompanyAkis {
+
+    // can be set inside the constructor but cannot be modified afterwards
+
+    uint16 public immutable startupYear;
+
+    constructor(uint16 _y) {
+
+        startupYear = _y;
+    }
+}
