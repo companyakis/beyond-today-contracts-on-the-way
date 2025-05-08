917b3d7d481d9ef6365ad6a22ecae7dd9ac615ef
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-08 09:45:35 +0300

    Update part-c-0001-primitive-data-types1.sol

--- part-c-0001-primitive-data-types1.sol
+++ part-c-0001-primitive-data-types1.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
 
+contract CompanyAkis {
+
+    bool public completed = false;
+
+    bool public started = true;
+
+    bool public ok;
+
+    constructor(bool _condition) {
+
+        ok = _condition;
+    }
+
+}
