619a7e360379dc9c3e9e9f610fada33f8f86794b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-08 09:58:04 +0300

    Create part-c-0001-primitive-data-types4.sol

---
+++ part-c-0001-primitive-data-types4.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract CompanyAkis {
+
+    // a random address from https://etherscan.io/
+    
+    address public random_address = 0xB9740184Cf59D5aa7b36Abf9Fc4A04e0635DAe4A;
+}
