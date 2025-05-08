fbbfc70b5436211b215cd6582eadd8d9a20fb060
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-08 10:26:43 +0300

    Create part-c-0003-constant.sol

---
+++ part-c-0003-constant.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract CompanyAkis {
+
+    // hard coded and can save gas!
+
+    string public constant founder = "Mustafa Kemal Ataturk";
+
+    uint16 public constant startupYear = 2025;
+}
