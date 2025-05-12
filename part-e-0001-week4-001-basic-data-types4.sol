679271950e43b98fbe9868261d42f5619ab52fab
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-12 19:43:04 +0300

    Create part-e-0001-week4-001-basic-data-types4.sol

---
+++ part-e-0001-week4-001-basic-data-types4.sol
@@ -0,0 +1,20 @@
+/*
+Your Goal: Hello World 
+It's time to do Hello World in Solidity! 
+
+Create a public bytes32 state variable msg1 which stores a string literal containing "Hello World".
+Create a public string state variable msg2 which stores a string literal that requires over 32 bytes to store.
+
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Contract {
+
+    bytes32 public msg1 = "Hello World";
+
+    string public msg2 = "Hello Hello Hello Hello Hello Hello Hello Hello";
+	
+}
+
