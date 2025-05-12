f451df3597a36e9cf71809887c8b3720d920a5be
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-12 23:45:02 +0300

    Create part-d-0009-variable-shadowing3.rs

---
+++ part-d-0009-variable-shadowing3.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+    let exam_result: u8 = 100;
+
+    println!("Exam result is {exam_result}.");
+
+    let exam_result: String = "awesome".to_string();
+
+    println!("Exam result is {exam_result}.")
+
+}
+
+// Exam result is 100.
+// Exam result is awesome.
