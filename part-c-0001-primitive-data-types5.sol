8bb9b7aeb2d98e2a4324ae39ec6ab28b7e656732
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-15 19:36:01 +0300

    Delete part-c-0001-primitive-data-types5.sol

--- part-c-0001-primitive-data-types5.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
-
-contract CompanyAkis {
-
-    string public name1 = "Mustafa Buyukdereli";
-
-    bytes32 public name2 = "Mustafa Gokturk Buyukdereli";
-}
