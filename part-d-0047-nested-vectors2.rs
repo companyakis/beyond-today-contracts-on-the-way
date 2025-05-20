09da3420f19f2dc9a6b9e4e9e40e69d7ef87be3e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-20 12:52:07 +0300

    Create part-d-0047-nested-vectors2.rs

---
+++ part-d-0047-nested-vectors2.rs
@@ -0,0 +1,12 @@
+fn main() {
+
+    let mut quarterly_sales_usd: Vec<Vec<u16>> = vec![vec![6250, 5400, 6870, 6980], vec![6500, 7200, 4700, 6400]];
+
+    quarterly_sales_usd.push(vec![9800, 6540, 4250, 6547]);
+
+    println!("{:?}", quarterly_sales_usd)
+
+}
+
+
+//[[6250, 5400, 6870, 6980], [6500, 7200, 4700, 6400], [9800, 6540, 4250, 6547]]
