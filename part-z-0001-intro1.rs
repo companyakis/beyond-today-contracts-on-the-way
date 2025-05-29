a1d53437d1d48b29abfd895cc25c90b2846374f1
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-29 21:00:43 +0300

    Create part-z-0001-intro1.rs

---
+++ part-z-0001-intro1.rs
@@ -0,0 +1,10 @@
+// fn main() {
+//     // TODO: Fix the code to print "Hello world!".
+//     printline!("Hello world!");
+// }
+
+fn main() {
+    // TODO: Fix the code to print "Hello world!".
+    println!("Hello world!");
+}
+
