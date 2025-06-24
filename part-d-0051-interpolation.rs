1a8a73983878a9c914122bcc3f0dbeaf056866ed
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-06-24 07:47:58 +0300

    Create part-d-0051-interpolation.rs

---
+++ part-d-0051-interpolation.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+    println!("In {1}, I will do my best! {0} will help me:)", "Solana", 2025); // In 2025, I will do my best! Solana will help me:)
+
+    let chain = "ICP";
+
+    let year: u16 = 2025;
+
+    println!("Not only Solana, but also {chain} will be helpful in {year}.") // Not only Solana, but also ICP will be helpful in 2025.
+
+}
+
+
+
