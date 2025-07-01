4fac037394d75766550441ad6319f656cf83bef5
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-01 13:46:34 +0300

    Create part-y-0001-getting-started2.rs

---
+++ part-y-0001-getting-started2.rs
@@ -0,0 +1,18 @@
+/*
+❓ Question 2: Adding a comment in Rust
+Write a Rust program that does the following:
+
+Add a single-line comment above the main function.
+The comment should describe what the program does.
+The main function should print This is a comment example. to the terminal.
+
+*/
+
+// main func
+
+fn main() {
+
+    println!("This is a comment example.")
+
+}
+
