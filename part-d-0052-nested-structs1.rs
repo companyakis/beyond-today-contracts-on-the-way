97e5975c846b6b90ac09ab7b056180bae9595189
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-14 08:19:55 +0300

    Create part-d-0052-nested-structs1.rs

---
+++ part-d-0052-nested-structs1.rs
@@ -0,0 +1,34 @@
+fn main() {
+  
+    // struct in struct
+
+    let citizen_ayhan = Person {
+      
+        name: String::from("Ayhan Bilir"),
+        country: String::from("Turkiye"),
+        birty_year: 1975,
+        work_info: ProLife {
+            company: "Pro Sales Inc".to_string(),
+            department: "Accounting".to_string(),
+            title: "Senior Accounting Expert".to_string(),
+            id: 4296,
+        },
+
+    };
+
+    println!("Employee Ayhan ID: {}", citizen_ayhan.work_info.id ) // Employee Ayhan ID: 4296
+}
+
+struct ProLife {
+    company: String,
+    department: String,
+    title: String,
+    id: u16,
+}
+
+struct Person {
+    name: String,
+    country: String,
+    birty_year: u16,
+    work_info: ProLife,
+}
