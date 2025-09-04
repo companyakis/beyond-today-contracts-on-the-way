3cf36adb2e876f2290c21dd38ad5574558708a8d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-09-04 12:34:00 +0300

    Create part-d-0007-functions00-cargo-doc.rs

---
+++ part-d-0007-functions00-cargo-doc.rs
@@ -0,0 +1,11 @@
+fn main() {
+
+
+}
+
+///this functions return multiple values
+/// sum, diff and mult results...
+fn return_multiple_values(a: i128, b: i128) -> (i128, i128, i128) {
+    
+    (a + b, a - b, a * b)
+}
