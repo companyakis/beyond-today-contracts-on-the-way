15ec6fc5024ecd5616a99d67f97a78aca5b9ffe9
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-09-09 17:17:58 +0300

    Create part-z-0003-scope2.rs

---
+++ part-z-0003-scope2.rs
@@ -0,0 +1,19 @@
+// Fix the error with the use of define_x
+fn main() {
+    let x = define_x();
+    println!("{}, world", x); 
+}
+
+fn define_x() -> &'static str {
+    let x = "hello";
+    x
+}
+
+// // Fix the error with the use of define_x
+// fn main() {
+//     println!("{}, world", x); 
+// }
+
+// fn define_x() {
+//     let x = "hello";
+// }
