7c80bfeaa2bb4150b9f7a7218e797f1da9e5ae33
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-09-10 18:51:07 +0300

    Create  part-z-0010-range1.rs

---
+++ part-z-0010-range1.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let mut sum = 0;
+    for i in -3..2 {
+        sum += i
+    }
+
+    assert!(sum == -5);
+
+    for i in 97..=122 {
+        println!("{}", i);
+    }
+}
+
+// // Two goals: 1. Modify assert! to make it work 2. Make println! output list of numbers between 97 and 122
+// fn main() {
+//     let mut sum = 0;
+//     for i in -3..2 {
+//         sum += i
+//     }
+
+//     assert!(sum == -3);
+
+//     for c in 'a'..='z' {
+//         println!("{}",c);
+//     }
+// }
