f796d46ef5ad91d23d67b73470c8834478388dd2
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-09-15 11:48:01 +0300

    Create part-z-0014-functions3.rs

---
+++ part-z-0014-functions3.rs
@@ -0,0 +1,26 @@
+// Solve it in two ways
+// DON'T let `println!` work
+fn main() {
+    never_return();
+
+    println!("Failed!");
+}
+
+fn never_return() -> ! {
+    // Implement this function, don't modify the fn signatures
+    panic!("I return nothing...")
+    
+}
+
+// // Solve it in two ways
+// // DON'T let `println!` work
+// fn main() {
+//     never_return();
+
+//     println!("Failed!");
+// }
+
+// fn never_return() -> ! {
+//     // Implement this function, don't modify the fn signatures
+    
+// }
